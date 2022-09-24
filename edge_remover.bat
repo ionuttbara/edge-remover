4ece46d705e4497ef83b8bc800e3880c47d99055
Joel Manuel <88746308+WindowsRefundDay@users.noreply.github.com>
2022-09-23 23:24:12 -0400

    option to use edge remover with or without backup
    work in progress

--- edge_remover.bat
+++ edge_remover.bat
@@ -2,6 +2,55 @@ pushd "%CD%"
 CD /D "%~dp0"
 @echo off
 @title Microsoft Edge Uninstaller
+
+		::Asks if user wants to make a backup or exit
+		
+		ECHO.
+		ECHO ..................................................
+		ECHO How do you want to start Microsoft Edge Uninstaller?
+		ECHO ..................................................
+		ECHO.
+		ECHO Uninstall Microsoft Edge and create a system restore point (recommended) (q)
+		ECHO Uninstall Microsoft Edge without creating system restore point (w)
+		ECHO Exit Microsoft Edge Uninstaller (e)
+		ECHO.
+
+		SET /P M=Type the letter next to your option then press ENTER: 
+		IF %M%==q goto startmsub
+		IF %M%==w goto startmsu
+		IF %M%==e (
+			cls
+			echo Leaving in 3s
+			timeout /t 1 /nobreak >nul 
+			cls
+			echo Leaving in 2s
+			timeout /t 1 /nobreak >nul
+			cls
+			echo Leaving in 1s
+			timeout /t 1 /nobreak >nul
+			exit /b
+			::Countdown
+			)
+			
+:startmsub
+echo Checking if launched with administrative permissions (Needed to create system restore point)
+    net session >nul 2>&1
+    
+    if %errorLevel% == 0 (
+        echo Administrative Permissions detected, now creating system restore point.
+        goto savebackup
+    ) else (
+        echo Failure: No administrative permissions. Please relaunch with Adnimistrative Permissions to create restore point.
+        pause
+        exit /b
+    )
+    
+:savebackup
+		cls
+		echo Making backup, please wait!
+		Wmic.exe /Namespace:\\root\default Path SystemRestore Call CreateRestorePoint "This_was_made_by_Edge_Remover_on_%DATE%", 100, 1 
+		echo Complete!
+:startmsu
 cd /d "%ProgramFiles(x86)%\Microsoft"
 for /f "tokens=1 delims=\" %%i in ('dir /B /A:D "%ProgramFiles(x86)%\Microsoft\Edge\Application" ^| find "."') do (set "edge_chromium_package_version=%%i")
 if defined edge_chromium_package_version (
@@ -22,7 +71,7 @@ if defined edge_legacy_package_version (
 	) else (
 		echo Microsoft Edge [Legacy/UWP] not found, skipping.
 	)
-echo Done. Press any key to exit.
+
 reg add "HKEY_LOCAL_MACHINE\SOFTWARE\Microsoft\EdgeUpdate" /v "DoNotUpdateToEdgeWithChromium" /t REG_DWORD /d 1
 cls
 echo Script has finished, press any key to exit
